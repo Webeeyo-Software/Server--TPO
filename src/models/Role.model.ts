789715d4c40452a726b5def68a444932d3ec527a
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-16 17:22:40 +0530

    Adding Role model

---
+++ src/models/Role.model.ts
@@ -0,0 +1,56 @@
+import { Model, DataTypes, Optional, Sequelize } from "sequelize";
+
+export interface RoleAttributes {
+  role_id: string;
+  role_name: string;
+}
+
+export interface RoleCreationAttributes
+  extends Optional<RoleAttributes, "role_id"> {}
+
+export class Role
+  extends Model<RoleAttributes, RoleCreationAttributes>
+  implements RoleAttributes
+{
+  public role_id!: string;
+  public role_name!: string;
+
+  static associate(models: any) {
+    if(models.UserRole){
+    Role.hasMany(models.UserRole, {
+      foreignKey: "role_id",
+      as: "userRoles", 
+    });
+  }
+  }
+  }
+
+
+export default function RoleFactory(sequelize: Sequelize): typeof Role {
+  Role.init(
+    {
+      role_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      role_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Role",
+      tableName: "roles",
+      freezeTableName: true,
+      timestamps: false,
+    }
+  );
+
+  return Role;
+}
