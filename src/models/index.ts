6315edc24a9af5332f27936632271abe7f6dc871
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-18 16:47:25 +0530

    improve index

--- src/models/index.ts
+++ src/models/index.ts
@@ -13,20 +13,22 @@ const sequelize = new Sequelize(dbConfig);
 
 // Read all model files in the current directory
 fs.readdirSync(__dirname)
-  .filter((file: string) => file !== 'index.ts')
-  .forEach((file: string) => {
-    const modelModule = require(path.join(__dirname, file));
-    const model = modelModule.default || modelModule; // handle both cases
-    if (typeof model === 'function') {
-      model(sequelize, DataTypes);
-    }
+  .filter((file: string) => {
+    return (
+      file.indexOf('.') !== 0 && 
+      file !== basename && 
+      file.slice(-3) === '.ts'
+    );
+  })
+  .forEach((file: any) => {
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+    db[model.name] = model;
   });
-
 
   // console.log("Here is the DB Object : ", db);
 
   
-//.
+
 // Set up associations between models
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
@@ -44,8 +46,8 @@ export const syncDatabase = async () => {
         await sequelize.authenticate();
         console.log('Database Connection has been established successfully.');
 
-        await sequelize.sync({force: false});
-        console.log('Database & tables created!');
+        // await sequelize.sync({force: false});
+        // console.log('Database & tables created!');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
         process.exit(1);
@@ -54,11 +56,3 @@ export const syncDatabase = async () => {
 
 export default db;
 export { sequelize };
-
-
-
-
-
-
-
-
