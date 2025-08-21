322d087250c626a5771fb0d8c6d4466779cdae2f
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-21 12:16:44 +0530

    Implement search functionality for companies with pagination and filtering options

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import router from './routers/profile/PersonalDetails.router';
 import Religionsrouter from './routers/profile/Religions.router';
 import DepartmentRouter from './routers/profile/Department.router';
 import BloodgroupRouter from './routers/profile/Bloodgroup.router';
+import companies from './routers/companies/companies'
 dotenv.config();
 
 const app: Application = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
+app.use('/api/companies', companies);
 app.use('/api/profile/personal-details', router);
 app.use('/api/profile/religions', Religionsrouter);
 app.use('/api/profile/departments', DepartmentRouter);
