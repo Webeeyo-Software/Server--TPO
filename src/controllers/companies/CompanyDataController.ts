b217974d7052808cbb64143016a81c8aa8ccf9ed
Kunalshejwal01 <kunalshejwal001@gmail.com>
2025-08-22 14:27:57 +0530

    refactor: improve error handling and response structure in getCompanyData

--- src/controllers/companies/CompanyDataController.ts
+++ src/controllers/companies/CompanyDataController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import db from "../../models";
-
+const Companies = db.Companies;
 const PlacementDrives = db.PlacementDrives;
 
 export const getCompanyData = async (req: Request, res: Response) => {
@@ -10,30 +10,45 @@ export const getCompanyData = async (req: Request, res: Response) => {
       return res.status(400).json({ success: false, message: "companyId is required in the URL" });
     }
 
-    const companyData = await PlacementDrives.findOne({
-      where: { companyId },
+    const companyDatafromCompanies = await Companies.findOne({
+      where: { id: companyId },
       attributes: [
-        "id",
-        "companyId",
-        "position",
-        "location",
-        "eligibilityCriteria",
-        "jobDescription",
-        "minPack",
-        "maxPack",
-        "ctc",
-        "driveDate",
-        "applicationDeadline"
+        'name', 
+        'description',
+        'website',
+        'email',
+        'contactNumber',
+        'location',
+        'logoUrl',
       ],
     });
 
-    if (!companyData) {
-      return res.status(400).json({ success: false, message: "No data found for that companyId" });
+    const companyDatafromPlacementDrives = await PlacementDrives.findOne({
+      where: { companyId },
+      attributes: [
+        'companyId',
+        'position',
+        'eligibilityCriteria',
+        'jobDescription',
+        'ctc',
+        'applicationDeadline',
+        'minPack',
+        'maxPack',
+      ],
+    });
+
+    if (!companyDatafromPlacementDrives && !companyDatafromCompanies) {
+      return res.status(404).json({ success: false, message: "No data found for that companyId" });
     }
+
+    const companyData = {
+      ...(companyDatafromCompanies ? companyDatafromCompanies.toJSON() : {}),
+      ...(companyDatafromPlacementDrives ? companyDatafromPlacementDrives.toJSON() : {})
+    };
 
     return res.status(200).json({ success: true, data: companyData });
   } catch (err: any) {
-    console.error(err);
+    console.error("Error in getCompanyData:", err);
     return res.status(500).json({
       success: false,
       message: "Internal server error while fetching company data",
