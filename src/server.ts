e6516bdb3db4e6223ae0e4d76649455382b4bda6
Aniruddha-2007 <bhagadeaniruddha3@gmail.com>
2025-08-24 11:48:35 +0530

    refactor: remove unused route imports and clean up server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,15 @@
 import express, {Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
 import sequelize, { syncDatabase } from './models';
-import router from './routers/profile/PersonalDetails.router';
-import Religionsrouter from './routers/profile/Religions.router';
-import DepartmentRouter from './routers/profile/Department.router';
-import BloodgroupRouter from './routers/profile/Bloodgroup.router';
-import companies from './routers/companies/companies';
 import TPORegistrations from "./routers/tpoRegistration/TPORegistration.routes";
-import notices from './routers/notices/Notice'
 dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
-app.use('/api/companies', companies);
-app.use('/api/profile/personal-details', router);
-app.use('/api/profile/religions', Religionsrouter);
-app.use('/api/profile/departments', DepartmentRouter);
-app.use('/api/profile/bloodgroups', BloodgroupRouter);
 app.use("/api/tpo-registrations", TPORegistrations );
-app.use("/api/notice", notices);
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
